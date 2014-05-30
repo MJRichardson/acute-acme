e2ef496593884b4ce37926015e0079fa25bfd9f4
MJ Richardson <michael.james.richardson@gmail.com>
2014-05-30 23:14:47 +1000

    Navigation

--- Acme.Client/AcmeApp.cs
+++ Acme.Client/AcmeApp.cs
@@ -1,6 +1,14 @@
-﻿namespace Acme.Client
+﻿using Acute;
+
+namespace Acme.Client
 {
     public class AcmeApp : Acute.App
     {
+        protected override void ConfigureRoutes(RouteProvider routeProvider)
+        {
+            routeProvider.When("/cart", new RouteConfig {TemplateUrl = "cart.html"});
+            routeProvider.Otherwise(new RouteConfig<ProductsController> {TemplateUrl = "products.html"});
+        }
     }
 }
+		
