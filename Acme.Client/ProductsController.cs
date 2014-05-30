7bb0d346f85c02cab0e4db99cc82b18065a3c46e
MJ Richardson <michael.james.richardson@gmail.com>
2014-05-30 14:38:18 +1000

    Created ProductsController

---
+++ Acme.Client/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Acute;
+
+namespace Acme.Client
+{
+    public class ProductsController : Controller
+    {
+        public override void Control(dynamic scope)
+        {
+	    scope.Products = new Product[]
+		{
+		   new Product{
+		       Id =1, 
+		       Name="Giant Rubber-Band", 
+		       Description = "Perfect for tripping Road Runners", 
+		       Price = 9.99m},  
+
+		   new Product{
+		       Id =2, 
+		       Name="Earthquake Pills", 
+		       Description = "Why wait? Make your own earthquakes - loads of fun!", 
+		       Price = 11.00m },  
+
+		   new Product{
+		       Id =3, 
+		       Name="Explosive Tennis Balls", 
+		       Description = "Tickle your friends! Surprise your opponent!", 
+		       Price = 25.40m }  
+		};
+        }
+    }
+}
